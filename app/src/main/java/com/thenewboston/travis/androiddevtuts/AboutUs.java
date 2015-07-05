bc9356f287c89fdb1618d9c29894fb84da506ee2
Sairaj4522 <sairaj4522@gmail.com>
2015-07-06 00:24:09 +0530

    first commit

---
+++ app/src/main/java/com/thenewboston/travis/androiddevtuts/AboutUs.java
@@ -0,0 +1,14 @@
+package com.thenewboston.travis.androiddevtuts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutUs extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about_us);
+	}
+
+}
---
+++ app/src/main/java/com/thenewboston/travis/androiddevtuts/StartingPoint.java
@@ -0,0 +1,47 @@
+package com.thenewboston.travis.androiddevtuts;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+public class StartingPoint extends Activity {
+
+	private int counter;
+	Button add, sub;
+	TextView display;
+
+	public void onCreate(Bundle savedInstaceState){
+		super.onCreate(savedInstaceState);
+		setContentView(R.layout.main);
+
+		counter = 0;
+		add = (Button) findViewById(R.id.bAdd);
+		sub = (Button) findViewById(R.id.bSub);
+		display = (TextView) findViewById(R.id.tvDisplay);
+
+
+		AdView mAdView = (AdView) findViewById(R.id.adView);
+		AdRequest adRequest = new AdRequest.Builder().build();
+		mAdView.loadAd(adRequest);
+
+		add.setOnClickListener(new View.OnClickListener(){
+						public void onClick(View view){
+							counter++;
+							display.setText("Your total is " + counter);
+						}			
+					}
+		);
+
+		sub.setOnClickListener(new View.OnClickListener(){
+			public void onClick(View view){
+				counter--;
+				display.setText("Your total is " + counter);
+			}
+		});
+	}
+}
